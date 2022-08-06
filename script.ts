7da8d5e32760fb735c7d3a9cb312c03118265d44
Semir Iljazi <iljazi.semir22@gmail.com>
2022-08-06 22:00:41 +0200

    First Commit

---
+++ script.ts
@@ -0,0 +1,25 @@
+let names : string
+
+let speed : number = 25
+
+let isLoading : boolean
+
+let age : string|number
+
+let colors : string[] = []
+
+colors.push('red')
+
+let infos : (string|number|boolean)[] = []
+
+infos.push("dupont", true, 40)
+
+let number : object = {}
+
+number = {
+    firstName : "Dupont",
+    age : 20,
+    isLoggedIn: true
+}
+
+console.log(number.age)
